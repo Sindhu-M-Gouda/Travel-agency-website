4cac5a74b49d0f6c3c5b73fe376de85a94b59dba
Sindhu-M-Gouda <sindhugouda03@gmail.com>
2025-02-25 10:35:58 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+var x;
+var y;
+var gender = "Male";
+var budget;
+if(budget == 900 && "Male")
+console.log("Bus");
+if(budget == 1000 || "Male")
+    console.log("Metro");
+if(!budget)
+    console.log("Bus");
+else
+console.log("Metro");
